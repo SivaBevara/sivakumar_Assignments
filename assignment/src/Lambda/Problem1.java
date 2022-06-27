c9e59354922f34bd943dfbd31b14bd48c1aa2acb
sibevara <sibevara@LIN24010725.corp.capgemini.com>
2022-06-27 14:10:25 +0530

    changes done

--- assignment/src/Lambda/Problem1.java
+++ assignment/src/Lambda/Problem1.java
@@ -6,19 +6,19 @@ public class Problem1{
 
 	public static void main(String[] args) {
 
-		//Lambda expression for addition
+		
 		BiFunction <Integer, Integer, Integer> funcAddObj = (i1, i2) -> i1 + i2;
 		
-		//Lambda expression for subtract
+		
 		BiFunction <Integer, Integer, Integer> funcSubtractObj = (i1, i2) -> i1 - i2;
 
-		//Lambda expression for multiply
+		
 		BiFunction <Integer, Integer, Integer> funcMuliplyObj = (i1, i2) -> i1 * i2;
 
-		//Lambda expression for division
+		
 		BiFunction <Integer, Integer, Integer> funcDivideObj = (i1, i2) -> i1 / i2;
 
-		//Lambda expression for division
+		
 		BiFunction <Integer, Integer, Integer> funcModuloObj = (i1, i2) -> i1 % i2;
 
 		System.out.println("Addition of 10 and 5: " + funcAddObj.apply(10, 5));
