90bba935ed2167c430f75c1f08387a3e35418733
sibevara <sibevara@LIN24010725.corp.capgemini.com>
2022-06-28 10:27:57 +0530

    .

--- assignment/src/java11/Problem3.java
+++ assignment/src/java11/Problem3.java
@@ -5,12 +5,6 @@ import java.util.Arrays;
 
 public class Problem3 {
     public static void main(String[] args) {
-        String str="A quick brown fox jumps over lazy dog";
-        ArrayList<String> strarr=new ArrayList<>();
-        //this will divide the string recursivly when it encounters a space
-        strarr.addAll(Arrays.asList(str.split(" ")));
-        System.out.println(strarr);
-        String[] array= strarr.toArray(new String[0]);
-        Arrays.stream(array).forEach(t-> System.out.println(t));
+       
     }
 }
